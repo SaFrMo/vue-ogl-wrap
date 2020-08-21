847a16a091563a9d788ebd0be5113f7a413fdbf2
Sander Moolin <sandermoolin@gmail.com>
2020-08-21 14:11:38 -0400

    vertex and fragment shaders

--- demo/App.vue
+++ demo/App.vue
@@ -1,17 +1,66 @@
 <template>
     <main class="vue-ogl-wrap-demo">
         <div class="wrapper">
-            <vue-ogl-wrap />
+            <vue-ogl-wrap @ogl-ready="init">
+                <script type="x-shader/x-fragment">
+                    precision highp float;
+                    uniform float uTime;
+                    uniform vec3 uColor;
+                    varying vec2 vUv;
+                    void main() {
+                        gl_FragColor.rgb = 0.5 + 0.3 * cos(vUv.xyx + uTime) + uColor;
+                        gl_FragColor.a = 1.0;
+                    }
+                </script>
+
+                <script type="x-shader/x-vertex">
+                    attribute vec2 uv;
+                    attribute vec2 position;
+                    varying vec2 vUv;
+                    void main() {
+                        vUv = uv;
+                        gl_Position = vec4(position, 0, 1);
+                    }
+                </script>
+            </vue-ogl-wrap>
         </div>
     </main>
 </template>
 
 <script>
 import VueOglWrap from '../src/VueOglWrap'
+import { Triangle, Program, Mesh, Color } from 'ogl'
 
 export default {
     components: {
         'vue-ogl-wrap': VueOglWrap
+    },
+    methods: {
+        init({ canvas, renderer, fragment, vertex }) {
+            const gl = renderer.gl
+
+            const geometry = new Triangle(gl)
+
+            const program = new Program(gl, {
+                vertex,
+                fragment,
+                uniforms: {
+                    uTime: { value: 0 },
+                    uColor: { value: new Color(0.3, 0.2, 0.5) }
+                }
+            })
+            const mesh = new Mesh(gl, { geometry, program })
+
+            requestAnimationFrame(update)
+            function update(t) {
+                requestAnimationFrame(update)
+
+                program.uniforms.uTime.value = t * 0.001
+
+                // Don't need a camera if camera uniforms aren't required
+                renderer.render({ scene: mesh })
+            }
+        }
     }
 }
 </script>
--- src/VueOglWrap.vue
+++ src/VueOglWrap.vue
@@ -1,18 +1,56 @@
 <template>
-    <full-canvas class="vue-ogl-wrap" />
+    <div class="vue-ogl-wrap">
+        <!-- the shader will go here -->
+        <slot />
+        <!-- and the result will be drawn here -->
+        <full-canvas class="vue-ogl-wrap" @canvas-ready="initCanvas" />
+    </div>
 </template>
 
 <script>
 import FullCanvas from '@fuzzco/full-canvas'
+import get from 'lodash/get'
+// prettier-ignore
+import { Renderer, Camera, Orbit, Transform, Geometry, Vec3, Color, Polyline } from 'ogl'
+
 export default {
+    props: {
+        fragment: { type: String, default: '' },
+        vertex: { type: String, default: '' }
+    },
     components: {
         'full-canvas': FullCanvas
+    },
+    methods: {
+        initCanvas(canvas) {
+            // fetch user-defined fragment shader
+            const fragmentVNode = get(this.$slots, 'default', []).find(
+                h => get(h, 'data.attrs.type', '') === 'x-shader/x-fragment'
+            )
+            const fragment = get(fragmentVNode, 'elm.innerHTML', this.fragment)
+
+            // fetch user-defined vertex shader
+            const vertexVNode = get(this.$slots, 'default', []).find(
+                h => get(h, 'data.attrs.type', '') === 'x-shader/x-vertex'
+            )
+            const vertex = get(vertexVNode, 'elm.innerHTML', this.vertex)
+
+            // initialize ogl
+            const renderer = new Renderer({ canvas })
+
+            this.$emit('ogl-ready', { canvas, renderer, fragment, vertex })
+        }
     }
 }
 </script>
 
 <style lang="scss">
-.vue-ogl-wrap {
-    background: red;
+.vue-ogl-wrap,
+.vue-ogl-wrap .full-canvas {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
 }
 </style>
