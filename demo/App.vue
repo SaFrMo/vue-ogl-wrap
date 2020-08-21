01321e02d4a74cfad2fa5b2ba749b9be69fc76a7
Sander Moolin <sandermoolin@gmail.com>
2020-08-21 13:45:44 -0400

    boilerplate

---
+++ demo/App.vue
@@ -0,0 +1,28 @@
+<template>
+    <main class="vue-ogl-wrap-demo">
+        <div class="wrapper">
+            <vue-ogl-wrap />
+        </div>
+    </main>
+</template>
+
+<script>
+import VueOglWrap from '../src/VueOglWrap'
+
+export default {
+    components: {
+        'vue-ogl-wrap': VueOglWrap
+    }
+}
+</script>
+
+<style lang="scss">
+.vue-ogl-wrap-demo {
+    .wrapper {
+        width: 400px;
+        height: 400px;
+        margin: 60px auto;
+        position: relative;
+    }
+}
+</style>
---
+++ src/VueOglWrap.vue
@@ -0,0 +1,18 @@
+<template>
+    <full-canvas class="vue-ogl-wrap" />
+</template>
+
+<script>
+import FullCanvas from '@fuzzco/full-canvas'
+export default {
+    components: {
+        'full-canvas': FullCanvas
+    }
+}
+</script>
+
+<style lang="scss">
+.vue-ogl-wrap {
+    background: red;
+}
+</style>
